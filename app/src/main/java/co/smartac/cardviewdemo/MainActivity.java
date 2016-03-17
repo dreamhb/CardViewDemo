d2bfbb2aae5821ded5cf2f6903643eeae4ec4bae
Jayden.hu <bing.hu1989@gmail.com>
2016-03-17 14:31:48 +0800

    other

--- app/src/main/java/co/smartac/cardviewdemo/MainActivity.java
+++ app/src/main/java/co/smartac/cardviewdemo/MainActivity.java
@@ -2,12 +2,16 @@ package co.smartac.cardviewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CardView cardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cardView = (CardView) findViewById(R.id.card_view);
     }
 }
